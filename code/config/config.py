d26a780c4ad5f4880b0fbac03f835fcce10b9764
sone <sone47.china@gmail.com>
2021-05-11 18:50:19 +0800

    add more dataset to config.py

--- code/config/config.py
+++ code/config/config.py
@@ -3,15 +3,27 @@
 import os
 
 dataset_name = {
-    'assist2009': 'assistment-2009-2010-skill'
+    'assist2009': 'assistment-2009-2010-skill',
+    'assist2012': 'assistment-2012-2013-non-skill',
+    'assist2015': 'assistment-2015'
 }
 
 dataset_dirname = {
-    'assist2009': '2009_skill_builder_data_corrected'
+    'assist2009': '2009_skill_builder_data_corrected',
+    'assist2012': '2012-2013-data-with-predictions-4-final',
+    'assist2015': '2015_100_skill_builders_main_problems'
 }
 
 dataset_filename = {
-    'assist2009': 'skill_builder_data_corrected.csv'
+    'assist2009': 'skill_builder_data_corrected.csv',
+    'assist2012': '2012-2013-data-with-predictions-4-final.csv',
+    'assist2015': '2015_100_skill_builders_main_problems.csv'
+}
+
+dataset_order_field = {
+    'assist2009': 'order_id',
+    'assist2012': 'end_time',
+    'assist2015': 'end_time'
 }
 
 num_questions = {
@@ -20,6 +32,7 @@ num_questions = {
 
 # data directory
 data_dir = os.path.join('/my project location.../KT-bias', 'data')
+# dataset = 'assist2012'
 dataset = 'assist2009'
 
 train_filename = 'train.txt'
